6f515902b54d22593140dfece14d58ccc8409b4e
comicodarko <bitotajima@hotmail.com>
2022-11-03 19:31:34 -0300

    ✅ - Server - Rotas iniciais

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john@doe.com',
+      avatarUrl: 'github.com/comicodarko.png'
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL123', 
+      ownerId: user.id,
+      
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-03T21:38:46.464Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-06T21:38:46.464Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 0,
+          
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main();
